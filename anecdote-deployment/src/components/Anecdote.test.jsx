6c394b5cee9d8ade95f012a5f3e02f3d8312e069
suyash bhardwaj <suyash_bhardwaj@rediffmail.com>
2025-04-29 13:07:07 +0530

    just a basic cleanup of comments etc

--- anecdote-deployment/src/components/Anecdote.test.jsx
+++ anecdote-deployment/src/components/Anecdote.test.jsx
@@ -1,9 +1,3 @@
-/*01 one of the default loaded anecdotes appear on screen (content & votes are mentioned)
-02 total number of anecdotes that appears on screen equal to the count of anecdotes saved to database
-03 typing in the search string in filter box populates the relevant anecdote(s)
-04 pressing the vote button raises the current value by one
-05 feeding in an anecdote content in create anecdote form makes it list in the populated anecdotes rightaway*/
-
 import { render, screen } from '@testing-library/react';
 import Anecdote from './Anecdote';
 import userEvent from '@testing-library/user-event';
